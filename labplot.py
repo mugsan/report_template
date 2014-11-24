54603db987a95543e39c2ad69cab5e2ef7250d9b
Magnus Björk <mugsan@gmail.com>
2014-11-24 10:12:17 +0100

    dojo but why not

--- labplot.py
+++ labplot.py
@@ -1,5 +1,5 @@
-from numpy import *
-from pylab import *
+import numpy as np
+import pylab as pl
 
 
 def length(x, h):
@@ -7,33 +7,36 @@ def length(x, h):
     return (a / h) * x ** 2
 
 
-x = linspace(0, 1.4, 10)  # list of x-values
+x = np.linspace(0, 1.4, 10)  # list of x-values
 y1 = length(x, 3)  # list of y-values of grade 1
 y2 = length(x, 5)  # list of y-values of grade 1
 y3 = length(x, 6)  # list of y-values of grade 1
 y4 = length(x, 8)  # list of y-values of grade 1
 
 # plot Graphs
-plot(x, y1, 'r-', label="3mm")
-plot(x, y2, 'b-', label="5mm")
-plot(x, y3, 'g-', label="6mm")
-plot(x, y4, 'm-', label="8mm")
+pl.figure()
+pl.plot(x, y1, 'r-', label="3mm")
+pl.plot(x, y2, 'b-', label="5mm")
+pl.plot(x, y3, 'g-', label="6mm")
+pl.plot(x, y4, 'm-', label="8mm")
+pl.savefig("./plot2.png")
+pl.close()
 
 # plot Points Length
-plot([0.4, 0.5, 0.6, 0.75, 0.8, 0.9, 1, 1.1, 1.2],
-     [0.083, 0.066, 0.094, 0.145, 0.164, 0.207, 0.254, 0.305, 0.362],
-     'bo',
-     label="5mm")
+pl.figure()
+pl.plot([0.4, 0.5, 0.6, 0.75, 0.8, 0.9, 1, 1.1, 1.2],
+        [0.083, 0.066, 0.094, 0.145, 0.164, 0.207, 0.254, 0.305, 0.362],
+        'bo',
+        label="5mm")
 
 # Thickness
-plot(1, 0.421, 'ro', label="3mm")
-plot(1, 0.215, 'go', label="6mm")
-plot(1, 0.162, 'mo', label="8mm")
+pl.plot(1, 0.421, 'ro', label="3mm")
+pl.plot(1, 0.215, 'go', label="6mm")
+pl.plot(1, 0.162, 'mo', label="8mm")
 
-xlabel("L(m)")
-ylabel("T(s)")
-legend(loc='upper left')
-grid(True)
-
-title("Svängningstid")
-savefig("./plot.png")
+pl.xlabel("L(m)")
+pl.ylabel("T(s)")
+pl.legend(loc='upper left')
+pl.grid(True)
+pl.savefig("plot.png")
+pl.close()
