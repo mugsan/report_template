5842b3a260eb7239a845696e1091637f998f66bf
Magnus Björk <mugsan@gmail.com>
2014-12-03 12:46:22 +0100

    added plot.py

---
+++ py/plot.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pylab as pl
+
+
+def length(x, h):
+    a = 1.27975
+    return (a / h) * x ** 2
+
+
+x = np.linspace(0, 1.4, 10)  # list of x-values
+y1 = length(x, 3)  # list of y-values of grade 1
+y2 = length(x, 5)  # list of y-values of grade 1
+y3 = length(x, 6)  # list of y-values of grade 1
+y4 = length(x, 8)  # list of y-values of grade 1
+
+# plot Graphs
+pl.figure()
+pl.plot(x, y1, 'r-', label="3mm")
+pl.plot(x, y2, 'b-', label="5mm")
+pl.plot(x, y3, 'g-', label="6mm")
+pl.plot(x, y4, 'm-', label="8mm")
+
+# plot Points Length
+pl.plot([0.4, 0.5, 0.6, 0.75, 0.8, 0.9, 1, 1.1, 1.2],
+        [0.083, 0.066, 0.094, 0.145, 0.164, 0.207, 0.254, 0.305, 0.362],
+        'bo',
+        label="5mm")
+
+pl.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+# Thickness
+pl.plot(1, 0.421, 'ro', label="3mm")
+pl.plot(1, 0.215, 'go', label="6mm")
+pl.plot(1, 0.162, 'mo', label="8mm")
+
+# Aluminum
+pl.plot(1, .252, 'y^', label="al 5mm")
+pl.plot(1, .232, 'g^', label="messing 8mm")
+
+pl.xlabel("L(m)")
+pl.ylabel("T(s)")
+pl.legend(loc='upper left')
+pl.grid(True)
+pl.savefig("plot.png")
+pl.show()
